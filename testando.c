bc710fb59193d7ca1dd83f17db43d0bd3b8aa718
GiovaniFRA <ferreiraalencoi1@gmail.com>
2022-09-17 22:36:48 -0300

    manipular mouse

--- testando.c
+++ testando.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
+#include <windows.h>
 
-     void reset(int *a){
-          *a=0;
-     }
+void limpartela(){
+    COORD coord;
+    DWORD written;
+    CONSOLE_SCREEN_BUFFER_INFO info;
 
-int main(){
-  
-   
+    coord.X = 0;
+    coord.Y = 0;
+    GetConsoleScreenBufferInfo ( GetStdHandle ( STD_OUTPUT_HANDLE ), &info );
+    FillConsoleOutputCharacter ( GetStdHandle ( STD_OUTPUT_HANDLE ), ' ',
+    info.dwSize.X * info.dwSize.Y, coord, &written );
+    SetConsoleCursorPosition ( GetStdHandle ( STD_OUTPUT_HANDLE ), coord );
+    return;
+}
+
+int main()
+{
+    int x, y;
+    char novamente;
+    do{
+        printf("Digite as coordenadas no formato x,y: ");
+        scanf("%d,%d", &x, &y);
+        SetCursorPos(x, y); //Muda a posição do cursor
+        printf("\nQuer testar novamente (s/n)? ");
+        scanf("\n%c", &novamente);
+        if (novamente == 's'){
+            limpartela(); //Limpa a tela se quiser testar novamente
+        }
+    }while(novamente == 's');
+    return 0;
 }
