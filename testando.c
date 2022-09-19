9367130080cf4ac59adaeae4f7bb0e3aa0de5f18
GiovaniFRA <ferreiraalencoi1@gmail.com>
2022-09-18 23:43:47 -0300

    automação ou quase

--- testando.c
+++ testando.c
@@ -1,33 +1,69 @@
 #include <stdio.h>
 #include <windows.h>
 
-void limpartela(){
-    COORD coord;
-    DWORD written;
-    CONSOLE_SCREEN_BUFFER_INFO info;
+void click(){
+     mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0,0,0);
+     mouse_event(MOUSEEVENTF_LEFTUP, 0, 0,0,0);
+     puts("clicou");
+}
 
-    coord.X = 0;
-    coord.Y = 0;
-    GetConsoleScreenBufferInfo ( GetStdHandle ( STD_OUTPUT_HANDLE ), &info );
-    FillConsoleOutputCharacter ( GetStdHandle ( STD_OUTPUT_HANDLE ), ' ',
-    info.dwSize.X * info.dwSize.Y, coord, &written );
-    SetConsoleCursorPosition ( GetStdHandle ( STD_OUTPUT_HANDLE ), coord );
-    return;
+void sendKeyDown(unsigned char keyCode)
+{
+    INPUT input;
+    input.type = INPUT_KEYBOARD;
+
+    input.ki.wVk = keyCode;
+    input.ki.dwFlags = 0;
+    input.ki.time = 0;
+    input.ki.dwExtraInfo = 0;
+
+    SendInput(1, &input, sizeof(INPUT));
+}
+
+void sendKeyUp(unsigned char keyCode)
+{
+    INPUT input;
+    input.type = INPUT_KEYBOARD;
+
+    input.ki.wVk = keyCode;
+    input.ki.dwFlags = KEYEVENTF_KEYUP;
+    input.ki.time = 0;
+    input.ki.dwExtraInfo = 0;
+
+    SendInput(1, &input, sizeof(INPUT));
+}
+
+void input(char a){
+    sendKeyDown(a);
+    sendKeyUp(a);
 }
 
 int main()
 {
-    int x, y;
-    char novamente;
-    do{
-        printf("Digite as coordenadas no formato x,y: ");
-        scanf("%d,%d", &x, &y);
-        SetCursorPos(x, y); //Muda a posição do cursor
-        printf("\nQuer testar novamente (s/n)? ");
-        scanf("\n%c", &novamente);
-        if (novamente == 's'){
-            limpartela(); //Limpa a tela se quiser testar novamente
-        }
-    }while(novamente == 's');
+        
+        SetCursorPos(1250,10); //Muda a posição do cursor  
+        click();
+        SetCursorPos(600,750);
+        click();
+        SetCursorPos(600,400);
+        click();
+        Sleep(100);
+        input('R');
+        input('O');
+        input('G');
+        input('E');
+        input('R');
+      
+        
+        // click();k
+
+    // while(1){
+    //      if(GetAsyncKeyState(0x01)){kk
+    //         puts("clicou");
+    //     }
+    // }
+       
+    
     return 0;
 }
+
